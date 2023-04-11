6364c69d710d608bd8ed6c9ff4bbea9269db581d
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-04-11 13:49:45 +0100

    api

---
+++ pages/api/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+
+export async function GET(request: Request) {
+  const data = fs.readFileSync("./public/data.json");
+
+  // Convertir le contenu JSON en un objet JavaScript
+  const jsonData = JSON.parse(data.toString());
+
+  console.log(jsonData);
+  return NextResponse.json({
+    status: 200,
+    data: jsonData,
+  });
+}
+
+export async function POST(request: Request) {
+  // Récupérer le corps de la requête POST
+  const req = await request.json();
+
+  // Ecrire les données dans le fichier JSON
+  fs.writeFileSync("./public/data.json", JSON.stringify(req));
+
+  // Retourner une réponse JSON
+  return NextResponse.json({
+    status: 200,
+    message: "Success",
+  });
+}
