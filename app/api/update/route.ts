c6f8fe0a73ad559c1e202c80de69dd4efcebf297
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-04-11 14:12:52 +0100

    commit

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
+import path from "path";
 
 export async function POST(request: Request) {
   const req = await request.json();
 
-  fs.writeFileSync("../public/data.json", JSON.stringify(req));
+  const filePath = path.join(process.cwd(), "public", "data.json");
+  fs.writeFileSync(filePath, JSON.stringify(req));
 
   return NextResponse.json({
     status: 200,
