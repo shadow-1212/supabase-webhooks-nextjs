681ab65337f09dd4a3bee7e3062c438fa9d15750
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-04-11 14:30:10 +0100

    commit

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,7 +5,7 @@ let data = { record: { email: "test12@gmail.com" } };
 export async function GET(request: Request) {
   return NextResponse.json({
     status: 200,
-    data,
+    last_user: data.record.email || null,
   });
 }
 
@@ -16,7 +16,6 @@ export async function POST(request: Request) {
 
   return NextResponse.json({
     status: 200,
-    last_user: data.record.email || null,
     message: "Data has been updated",
   });
 }
