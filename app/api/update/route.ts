ed71216b24f90f64c8ff24897cbf0e01c6946beb
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-04-11 14:15:32 +0100

    commit

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -5,7 +5,15 @@ import path from "path";
 export async function POST(request: Request) {
   const req = await request.json();
 
-  const filePath = path.join(process.cwd(), "public", "data.json");
+  const filePath = path.join(process.cwd(), "data.json");
+
+  if (!fs.existsSync(filePath)) {
+    return NextResponse.json({
+      status: 404,
+      message: "File does not exist.",
+    });
+  }
+
   fs.writeFileSync(filePath, JSON.stringify(req));
 
   return NextResponse.json({
