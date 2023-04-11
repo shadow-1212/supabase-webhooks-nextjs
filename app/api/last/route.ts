0e7434a207eb28ff26ec5d4e2157184755a7dd52
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-04-11 11:25:32 +0100

    first

--- app/api/last/route.ts
+++ app/api/last/route.ts
@@ -10,6 +10,20 @@ export async function GET(request: Request) {
   console.log(jsonData);
   return NextResponse.json({
     status: 200,
-    data: jsonData
+    data: jsonData,
+  });
+}
+
+export async function POST(request: Request) {
+  // Récupérer le corps de la requête POST
+  const req = await request.json();
+
+  // Ecrire les données dans le fichier JSON
+  fs.writeFileSync("./public/data.json", JSON.stringify(req));
+
+  // Retourner une réponse JSON
+  return NextResponse.json({
+    status: 200,
+    message: "Success",
   });
 }
