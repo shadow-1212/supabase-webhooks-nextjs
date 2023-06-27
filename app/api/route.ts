6a3160e56579e7617a468b9f9578ebc6e6af486f
ANDRIANARIVELO Sitrakarinjaka Tovonirina <sitrakarinjakatovonirina@gmail.com>
2023-06-27 09:42:08 +0300

    some changes

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,7 +5,7 @@ let data = { record: { email: "test12@gmail.com" } };
 export async function GET(request: Request) {
   return NextResponse.json({
     status: 200,
-    last_user: data.record.email || null,
+    data,
   });
 }
 
@@ -16,6 +16,7 @@ export async function POST(request: Request) {
 
   return NextResponse.json({
     status: 200,
+    data,
     message: "Data has been updated",
   });
 }
